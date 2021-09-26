66cc2621120117aa4a2251365d9e3183c6fbd905
Daniel Moura <daniel.mlds@gmail.com>
2021-09-26 00:39:54 -0300

    Configuring hr-config-server project

--- hr-config-server/src/main/java/br/com/danimlds/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/br/com/danimlds/hrconfigserver/HrConfigServerApplication.java
@@ -2,7 +2,11 @@ package br.com.danimlds.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
